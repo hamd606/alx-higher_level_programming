ce7ad319a6ec2c5a5e27224cc4facc70e6bad730
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-08-14 05:10:02 -0700

    auto commi

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,11 +4,11 @@ def divisible_by_2(my_list=[]):
     output = []
     for i in my_list:
         if i % 2 == 0:
-            print("{:d} is divisible by 2".format(i))
+            #print("{:d} is divisible by 2".format(i))
             output.append(True)
 
         else:
-            print("{:d} is not divisible by 2".format(i))
+            #print("{:d} is not divisible by 2".format(i))
             output.append(False)
 
     return output
