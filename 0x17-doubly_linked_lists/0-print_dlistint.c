28a61d3a03b639ef6e15a0656a11c71955862b1f
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-08-17 10:36:48 -0700

    constructing ...

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints the length of a dlistint
+ * @h: input list (dlistint object)
+ * Return: the length of h as a size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+
+	}
+
+	return (len);
+
+}
+
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+C Dojubly linked list
