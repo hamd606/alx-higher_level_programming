2490bc9e672d14d4f9e5e475cb13a9ba9a6917f5
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-08-29 06:32:01 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    output = 0
+    count = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            output += 1
-        except:
-            continue
-
-        print()
-
-        return output
+        except Exception as e:
+            break
+        else:
+            count += 1
+    print()
+    return count
