301c641832777deee17f45954d076d92579841d6
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-02 06:19:08 -0700

    Constructing...

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,7 +14,8 @@ class Square():
             raise ValueError("size must be >= 0")
 
     def area(self):
-        return self._size ** 2
+        """area getter"""
+        return self.__size ** 2
 
     @property
     def size(self):
