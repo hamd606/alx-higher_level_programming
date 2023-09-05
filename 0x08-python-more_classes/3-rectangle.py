f73a23ceb2a4b0af4b5a43be87bdb0cfcb3b397b
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-05 01:57:47 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,4 +52,4 @@ class Rectangle():
             for j in range(self.__width):
                 graphical_rec += "#"
             graphical_rec += "\n"
-        return graphical_rec[:len(graphical_re) - 2]
+        return graphical_rec[:len(graphical_rec) - 1]
