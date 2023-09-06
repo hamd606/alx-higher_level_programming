513c2691087a2589ce5292c11e52176679c44c16
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-06 13:33:08 -0700

    Still going with tasks....

--- 0x08-python-more_classes/8-main.py
+++ 0x08-python-more_classes/8-main.py
@@ -16,4 +16,7 @@ if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
     print("my_rectangle_1 is bigger or equal to my_rectangle_2")
 else:
     print("my_rectangle_2 is bigger than my_rectangle_1")
+myrectangle1 = Rectangle(8, 4)
+myrectangle2 = Rectangle(2, 3)
 
+print(myrectangle1 == Rectangle.bigger_or_equal(myrectangle1, myrectangle2))
--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,5 +75,5 @@ class Rectangle():
         if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() > rect_2.area() or rect_1.area() == rect_2.area():
-            return rect_1.area()
-        return rect_2.area()
+            return rect_1
+        return rect_2
