9dd080fb784908cc91af9e133d2602a7dad203f4
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-11 12:54:23 -0700

    mor classes

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module has a funcction that check if a class is instance"""
+
+
+def is_same_class(obj, a_class):
+    """his function check if obj is an istannce of a a_class"""
+    return isinstance(obj, a_class)
