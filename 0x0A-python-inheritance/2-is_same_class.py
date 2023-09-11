e802ae162dae65118d7b7b19d0c3ae207b7a4a23
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-11 12:51:06 -0700

    test

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module has a funcction that check if a class is instance"""
+
+
+def is_same_class(obj, a_class):
+    """his function check if obj is an istannce of a a_class"""
+    return type(obj) is a_class
