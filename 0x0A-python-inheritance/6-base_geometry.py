2065ff6e3138b6ffc5647dc53b96ac661fe6391f
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-11 13:05:09 -0700

    mor classes

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""empty class named geo..."""
+
+
+class BaseGeometry():
+    """empty class"""
+    
+    @classmethod
+    def area(self):
+        """calculated area"""
+
+        raise Exception("area() is not implemented")
