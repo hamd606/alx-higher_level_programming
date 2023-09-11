9051d082c4b088271185c6fc6569017e6d259042
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-11 12:06:36 -0700

    task12

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module has a funcction that prints a class attributes"""
+
+
+def lookup(obj):
+    """his function prints obj attributes"""
+    print(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+"""this is a class inheritance f-rom list class"""
+
+
+class MyList(list):
+    """class that extends the list class"""
+    def print_sorted(self):
+        """a function that prints a sorted list"""
+        print(sorted(self))
+
