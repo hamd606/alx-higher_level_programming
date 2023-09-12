06aac455389f6f17aa5949c371952361998c02c7
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-12 15:25:00 -0700

    task

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+import io
+
+size = 0
+dictionary_status = {}
+count = 0
+for line in sys.stdin:
+    split = line.split()
+    status = split[-2]
+    size += int(split[-1])
+    if status in dictionary_status.keys():
+        dictionary_status[status] += 1
+    else:
+        dictionary_status[status] = 1
+    count += 1
+    if count == 10:
+        sortme = sorted(dictionary_status.keys())
+        print("File size:", size)
+        for keys in sortme:
+            print("{}: {}".format(keys, dictionary_status[keys]))
+        count = 0
+        continue
