ec4ab06ded3fa0453c8953657bd6486055dbb8b9
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-12 14:24:24 -0700

    task

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that tests json module"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return json.dumps(my_obj)
