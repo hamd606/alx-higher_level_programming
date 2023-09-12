7c3e91ad384a3d66e77c7096470a37b0f8fb3b19
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-12 10:34:16 -0700

    test

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module has function that do stuff
+I believe that the dowcs will just waste your time it"s very simple"""
+
+
+def read_file(filename=""):
+    """simple enough just look at the code"""
+    f = open(filename, 'r')
+    print(f.read())
+    return f
