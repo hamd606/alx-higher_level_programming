9f8179e4a2869cec585dc9d553d3d857c243d1e0
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-19 12:18:19 -0700

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,40 @@ class Base():
     @classmethod
     def current_nb_obj(cls):
         return int(cls.__nb_objects)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open(f"{cls.__class__}.json", "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from json to dict"""
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creates an instance from dictionary"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new_obj = Rectangle(2, 2)
+        elif cls is Square:
+            new_obj = Square(2)
+        else:
+            new = None
+        new_obj.update(**dictionary)
+        return new_obj
