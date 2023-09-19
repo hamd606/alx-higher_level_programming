3eed65803db300cc959f1e757bd74d378f761325
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-19 10:50:51 -0700

    tabs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,8 +91,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """str formatting"""
-        return f"[Rectangle] ({self.id}) \
-                {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
     def update_helper(self, id=None, width=None, height=None, x=None, y=None):
         """updates attrib (helper func for update func"""
