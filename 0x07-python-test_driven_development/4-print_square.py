895020c5b6a06aadbd58832a4e63786b3b2b9d4b
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-29 16:21:37 -0700

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""prints a square"""
+
+
+def print_square(size):
+    """prints a square"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for index in range(size):
+        print("x" * size)
