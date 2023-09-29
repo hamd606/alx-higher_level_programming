687924efdc78f0a1515d6449727acdb867afd6cb
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-29 16:14:09 -0700

    fixing

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,16 +7,19 @@ matrix_devided([2,4,6], 2)
 def matrix_divided(matrix, div):
     """devides a matrix by a number"""
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
 
     rows_len = []
     for i in matrix:
         if type(i) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) \
+    of integers/floats")
         rows_len.append(len(i))
         for j in i:
             if type(j) != int and type(j) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
 
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
@@ -24,9 +27,8 @@ def matrix_divided(matrix, div):
     for p in rows_len:
         if p != rows_len[0]:
             raise TypeError("Each row of the matrix must have the same size")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     return [[round(item / div, 2) for item in row] for row in matrix]
-
