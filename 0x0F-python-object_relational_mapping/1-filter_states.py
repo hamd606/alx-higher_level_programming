0d94cd34b827f47277468261a725ece058346f89
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-11-21 09:48:17 -0800

    cmt

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""# Lists all states with a name starting 
+"""# Lists all states with a name starting
 # with letter "N" from the database hbtn_0e_0_usa
 # Usage: ./1-filter_states.py <mysql username> \
 #               <mysql password> <database name>"""
