fa2081f864cc8da5180ac4f8eef0c2e03a9d13e3
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-12-02 00:27:40 -0800

    tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me, gets the response You got me!
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "origin:HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
